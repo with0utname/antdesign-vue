2e946000f3ee2bf5d835c1820e60ae5d60018864
aaaaalang <742726301@qq.com>
2020-10-12 11:09:17 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div style="margin: 24px">
+    <p style="margin-bottom: 24px">
+      Current ant-design-vue version: {{ version }} <br />
+      You can change ant-design-vue version on the left panel (Dependencies
+      section).
+    </p>
+    <a-date-picker :defaultValue="moment()" />
+  </div>
+</template>
+
+<script>
+import moment from "moment";
+import { version } from "ant-design-vue";
+export default {
+  name: "App",
+  data() {
+    return {
+      moment,
+      version
+    };
+  }
+};
+</script>
+
+<style></style>
